aef7000b5eafda4335335bda709c6bd74ec49e0a
Mohammad Azam <azamsharp@gmail.com>
2021-08-09 10:17:57 -0500

    Initial Commit

---
+++ XcodersGraphQL/XcodersGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodersGraphQLApp.swift
+//  XcodersGraphQL
+//
+//  Created by Mohammad Azam on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct XcodersGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
